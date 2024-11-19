334fa03a3bb70abf9ed076d8e176273441f7cfa9
g <144202456+cooki-byte@users.noreply.github.com>
2024-11-19 14:34:13 -0500

    Initial project setup

---
+++ src/main/java/cop4331/App.java
@@ -0,0 +1,13 @@
+package cop4331;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/cop4331/client/User.java
@@ -0,0 +1,8 @@
+package cop4331.client;
+
+/**
+ * @author 
+ */
+public class User {
+
+}
